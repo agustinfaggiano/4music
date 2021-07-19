28441ee9ef9ebff8be5c4dc4b7fbea249290a81c
Agustin Faggiano <agustinfaggiano>
2021-07-19 11:40:25 -0300

    Sprint 3

--- app.js
+++ app.js
@@ -2,26 +2,29 @@ const express = require ("express");
 const app = express();
 const path = require ("path");
 
+// Routers:
+const mainRouter = require("./src/routes/mainRouter");
+const usersRouter = require("./src/routes/userRouter");
+const productsRouter = require("./src/routes/productRouter");
+const cartRouter = require("./src/routes/cartRouter");
+
+
 let rutaCarpetaPublic = path.resolve(__dirname,"./public"); //genero ruta absoluta
 app.use(express.static(rutaCarpetaPublic)); //indico los archivos estáticos publicos
 
+app.set("view engine", "ejs");
 
 app.listen(process.env.PORT || 3000,function(){
     console.log("Servidor corriendo en Puerto 3000");
 });
 
-app.get("/", (req,res)=> {
-    res.sendFile(path.resolve(__dirname,"./views/index.html"));
-});
-app.get("/login", (req,res)=> {
-    res.sendFile(path.resolve(__dirname,"./views/login.html"));
-});
-app.get("/register", (req,res)=> {
-    res.sendFile(path.resolve(__dirname,"./views/register.html"));
-});
-app.get("/cart", (req,res)=> {
-    res.sendFile(path.resolve(__dirname,"./views/cart.html"));
-});
-app.get("/item-detail", (req,res)=> {
-    res.sendFile(path.resolve(__dirname,"./views/item-detail.html"));
-});
+
+// Rutas Globales:
+
+app.use("/", mainRouter);
+
+app.use("/products", productsRouter);
+
+app.use("/users", usersRouter);
+
+app.use("/cart", cartRouter);
